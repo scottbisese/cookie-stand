85962a0767723b25199ac5483f58c6b832cfea05
Grimly Wallace <grimlywallace@Grimlys-MacBook-Air.local>
2019-07-23 08:36:22 -0500

    got lab 6 finished with object literals.. but it doesnt work yet

--- js/app.js
+++ js/app.js
@@ -2,94 +2,130 @@
 //hour array
 var hours=['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-var Pike={
+var Pike={          //create five object literals with random customer and cookies purchased data
     minCust: 23,
     maxCust: 65,
     avgCooks: 6.3,
     hourArray: [],
-    numCust: function(){
-        return math.ceil(math.random()*(Pike.maxCust-Pike.minCust + 1) + Pike.minCust);
-    },
-    cooksPurchased: function(){
-        for(var i=0; i<15; i++){
-            Pike.hourArray[i]= math.floor(this.numCust()*Pike.avgCooks);
+    randomCust: function(){
+        Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
+        return this.randomCust;
+    },                                     //everybody was using this method of getting a good random num.
+        cooksPurchased = function() {
+            for (var i=0; i<15; i++){
+            this.hourArray[i] = Math.floor(this.randomCust() * this.avgCooks);
+            };
         }
-    },
 };
+
 
 var SeaTac={
     minCust: 3,
     maxCust: 24,
     avgCooks: 1.2,
     hourArray: [],
-    numCust: function(){
-        return (math.random()*(SeaTac.maxCust-SeaTac.minCust) + SeaTac.minCust);
+    randomCust: function(){
+        return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
     },
-    cooksPurchased: function(){
-        for(var i=0; i<15; i++){
-            SeaTac.hourArray[i]= math.floor(this.numCust()*SeaTac.avgCooks);
+        cooksPurchased = function() {
+            for (var i=0; i<15; i++){
+            this.hourArray[i] = Math.floor(this.randomCust() * this.avgCooks);
+            };
         }
-    },
 };
+
 
 var SeattleCenter={
     minCust: 11,
     maxCust: 38,
     avgCooks: 3.7,
     hourArray: [],
-    numCust: function(){
-        return (math.random()*(SeattleCenter.maxCust-SeattleCenter.minCust) + SeattleCenter.minCust);
+    randomCust: function(){
+        return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
     },
-    cooksPurchased: function(){
-        for(var i=0; i<15; i++){
-            SeattleCenter.hourArray[i]= math.floor(this.numCust()*SeattleCenter.avgCooks);
-        }
-    },
+        cooksPurchased = function() {
+            for (var i=0; i<15; i++){
+            this.hourArray[i] = Math.floor(this.randomCust() * this.avgCooks);
+            };
+    }
 };
+
 
 var CapitolHill={
     minCust: 20,
     maxCust: 38,
     avgCooks: 2.3,
     hourArray: [],
-    numCust: function(){
-        return (math.random()*(CapitolHill.maxCust-CapitolHill.minCust) + CapitolHill.minCust);
+    randomCust: function(){
+        return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
     },
-    cooksPurchased: function(){
-        for(var i=0; i<15; i++){
-           CapitolHill.hourArray[i]= math.floor(this.numCust()*CapitolHill.avgCooks);
+        cooksPurchased = function() {
+            for (var i=0; i<15; i++){
+            this.hourArray[i] = Math.floor(this.randomCust() * this.avgCooks);
+            };
         }
-    },
 };
+
 
 var Alki={
     minCust: 2,
     maxCust: 16,
     avgCooks: 4.6,
     hourArray: [],
-    numCust: function(){
-        return math.ceil(math.random()*(Alki.maxCust-Alki.minCust + 1) + Alki.minCust);
+    randomCust: function(){
+        return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
     },
-    cooksPurchased: function(){
-        for(var i=0; i<15; i++){
-           Alki.hourArray[i]= math.floor(this.numCust()*Alki.avgCooks);
+        cooksPurchased = function() {
+            for (var i=0; i<15; i++){
+            this.hourArray[i] = Math.floor(this.randomCust() * this.avgCooks);
+            };
         }
-    },
 };
-function simulate() {
-    Pike.numCust();
-    Pike.cooksPurchased();
-    SeaTac.numCust();
-    SeaTac.cooksPurchased();
-    SeattleCenter.numCust();
-    SeattleCenter.cooksPurchased();
-    CapitolHill.numCust();
-    CapitolHill.cooksPurchased();
-    Alki.numCust();
-    Alki.cooksPurchased();
+
+Pike.cooksPurchased();                  //call the function to generate random sales data per store
+SeaTac.cooksPurchased();               // and populate the hourArray for each object literal
+SeattleCenter.cooksPurchased();
+CapitolHill.cooksPurchased();
+Alki.cooksPurchased();
+
+function MakeList (location, id) {                      //use JS to create html lists
+    var ListLocation = document.getElementById(id);    // create variable to store dom id
+    for (var i = 0;i<hours.length;i++){               // iterate through hour array at very top
+        var sales = document.createElement('li');    // create a new variable sales that is a list element
+        var message = '${hours[i]}: ${location.hoursArray[i]} cookies'; //display a message
+        sales.textcontent = message;                                   // append the child 
+        ListLocation.appendChild(sales);    }
 }
 
-simulate();
-console.log(Pike);
+var totals = document.createElement('li');
+    var total = 0;
+        for (var i = 0; i<hours.length;i++){
+            total += location.hourArray[i];
+        }
+    var totalMessage = 'Total: ${total} cookies';
+    totals.textContent = totalMessage;
+    ListLocation.appendChild(totals);
+    
+MakeList(Pike,"PikeList");
+MakeList(SeaTac,"SeaTacList");
+MakeList(SeattleCenter, "SeattleCenterList");
+MakeList(CapitolHill, "CapitolHillList");
+MakeList(Alki, "AlkiList");
+
+// function simulate() {
+//     Pike.numCust();
+//     Pike.cooksPurchased();
+//     SeaTac.numCust();
+//     SeaTac.cooksPurchased();
+//     SeattleCenter.numCust();
+//     SeattleCenter.cooksPurchased();
+//     CapitolHill.numCust();
+//     CapitolHill.cooksPurchased();
+//     Alki.numCust();
+//     Alki.cooksPurchased();
+// }
+
+// simulate();
+// console.log(Pike);
 
 //had some help from Rebel Flesh, but i typed it all out and changed values. Still.. it didn't work.. but I have run out of time. 
